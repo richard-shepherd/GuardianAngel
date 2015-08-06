ed56108bbde3e6e831accfb50dd2e3c350164505
richard <richard.s.shepherd@gmail.com>
2015-08-06 23:27:10 +0100

    ga

---
+++ www/js/GuardianAngel.js
@@ -0,0 +1,21 @@
+/**
+ * GuardianAngel
+ * -------------
+ * Code to manage the Guardian Angel app.
+ */
+
+/**
+ * @constructor
+ */
+function GuardianAngel() {
+    alert("Hello");
+}
+
+// An enum for the slides we show...
+GuardianAngel.Slide = {
+    SETTINGS: 0,
+    LEAN_ANGLE: 1,
+    MAPS: 2,
+    LOGS: 3
+};
+
