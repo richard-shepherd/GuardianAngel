64cf9b20d76f071763e0932dc314a192da9c267c
richard <richard.s.shepherd@gmail.com>
2015-08-07 09:31:37 +0100

    ga

--- www/js/GuardianAngel.js
+++ www/js/GuardianAngel.js
@@ -8,11 +8,20 @@
  * @constructor
  */
 function GuardianAngel() {
-    this._swiper = new Swiper('.swiper-container', {
-        initialSlide: 1,
-        pagination: '.swiper-pagination',
-        paginationClickable: true
-    });
+    try{
+        // We create the "swiper" which shows the slides...
+        this.swiper = new Swiper('.swiper-container', {
+            initialSlide: 1,
+            pagination: '.swiper-pagination',
+            paginationClickable: true
+        });
+
+        // The Raphael object used to draw the lean-angle dial...
+        this.raphaelCanvas = null;
+        this.showLeanAngleDial();
+    } catch(err) {
+        alert(err.message);
+    }
 }
 
 // An enum for the slides we show...
@@ -21,5 +30,32 @@ GuardianAngel.Slide = {
     LEAN_ANGLE: 1,
     MAPS: 2,
     LOGS: 3
+};
+
+/**
+ * showLeanAngleDial
+ * -----------------
+ * Shows the lean-angle dial.
+ */
+GuardianAngel.prototype.showLeanAngleDial = function() {
+    var leanAngle_Width = $("#lean-angle-dial").width();
+    var leanAngle_Height = $("#lean-angle-dial").height();
+    var dialRadius = leanAngle_Width / 3.0;
+    var offsetX = leanAngle_Width / 2.0;
+    var offsetY = leanAngle_Width / 2.0;
+    var centerRadius = leanAngle_Width / 40.0;
+    var labelOffset = leanAngle_Width / 20.0;
+    var labelFontSize = leanAngle_Width / 20.0;
+
+    this.raphaelCanvas = Raphael("lean-angle-dial", leanAngle_Width, leanAngle_Height);
+    var leanAngleDial = new wso2vis.ctrls.CGauge()
+        .dialRadius(dialRadius)
+        .smallTick(2) .largeTick(10)
+        .minVal(-60) .maxVal(60)
+        .minAngle(90).maxAngle(270)
+        .ltlen(18) .stlen(15)
+        .needleCenterRadius(centerRadius) .needleBottom(centerRadius * 2.0)
+        .labelOffset(labelOffset) .labelFontSize(labelFontSize)
+        .create(this.raphaelCanvas, offsetX, offsetY);
 };
 
