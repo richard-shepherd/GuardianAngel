a51eba3f8d0279175c250467d8c8623139dbc670
richard <richard.s.shepherd@gmail.com>
2015-08-07 19:25:42 +0100

    ga

--- www/js/GuardianAngel.js
+++ www/js/GuardianAngel.js
@@ -9,18 +9,50 @@
  */
 function GuardianAngel() {
     try{
+        this.log("Guardian Angel starting.");
+
+        var that = this;
+
+        // True if the ride has started, ie the Start button has been pressed...
+        this.rideStarted = false;
+
+        // The lean angle when the Start button is pressed. We show lean angles
+        // relative to this, to compensate for the device not being exactly straight.
+        this.centeredLeanAngle = 0.0;
+
+        // We hold the last lean-angle shown in the dial, and a movement tolerance. This
+        // avoids redrawing the dial for small movements...
+        this.previousLeanAngleDisplayed = 999.9;
+        this.leanAngleDialSignificantChange = 2.0;
+
+        // The maximum lean angles recorded in a session...
+        this.maxLeftLean = 0.0;
+        this.maxRightLean = 0.0;
+
         // We create the "swiper" which shows the slides...
-        this.swiper = new Swiper('.swiper-container', {
-            initialSlide: 1,
-            pagination: '.swiper-pagination',
-            paginationClickable: true
-        });
+        this.swiper = null;
+        this.createSwiper();
 
         // The Raphael object used to draw the lean-angle dial...
         this.raphaelCanvas = null;
-        this.showLeanAngleDial();
-    } catch(err) {
-        alert(err.message);
+        this.leanAngleDial = null;
+        this.createLeanAngleDial();
+
+        // We start the lean-angle calculator.
+        // This subscribes to the lean-angle and smooths it...
+        this.log("Creating the lean-angle calculator.");
+        this.leanCalculator = new LeanCalculator(
+            function(leanAngle) { that.onLeanAngleUpdated(leanAngle); },
+            function() { that.onCrashDetected(); },
+            {} // optional params
+        );
+
+        // We register the Start button click...
+        this.log("Registering Start button click event.")
+        $("#start-button").click(function() { that.onStartButtonClicked(); });
+
+        } catch(err) {
+        this.error(err.message);
     }
 }
 
@@ -28,16 +60,32 @@ function GuardianAngel() {
 GuardianAngel.Slide = {
     SETTINGS: 0,
     LEAN_ANGLE: 1,
-    MAPS: 2,
+    RIDE_INFO: 2,
     LOGS: 3
 };
 
 /**
- * showLeanAngleDial
+ * createSwiper
+ * ------------
+ * Creates the object which manages the sliding "windows" for the app.
+ */
+GuardianAngel.prototype.createSwiper = function() {
+    this.log("Creating swiper layout.");
+    this.swiper = new Swiper('.swiper-container', {
+        initialSlide: GuardianAngel.Slide.LEAN_ANGLE,
+        pagination: '.swiper-pagination',
+        paginationClickable: true
+    });
+};
+
+/**
+ * createLeanAngleDial
  * -----------------
  * Shows the lean-angle dial.
  */
-GuardianAngel.prototype.showLeanAngleDial = function() {
+GuardianAngel.prototype.createLeanAngleDial = function() {
+    this.log("Creating the lean-angle dial.");
+
     var leanAngle_Width = $("#lean-angle-dial").width();
     var leanAngle_Height = $("#lean-angle-dial").height();
     var dialRadius = leanAngle_Width / 3.0;
@@ -48,7 +96,7 @@ GuardianAngel.prototype.showLeanAngleDial = function() {
     var labelFontSize = leanAngle_Width / 20.0;
 
     this.raphaelCanvas = Raphael("lean-angle-dial", leanAngle_Width, leanAngle_Height);
-    var leanAngleDial = new wso2vis.ctrls.CGauge()
+    this.leanAngleDial = new wso2vis.ctrls.CGauge()
         .dialRadius(dialRadius)
         .smallTick(2) .largeTick(10)
         .minVal(-60) .maxVal(60)
@@ -59,3 +107,131 @@ GuardianAngel.prototype.showLeanAngleDial = function() {
         .create(this.raphaelCanvas, offsetX, offsetY);
 };
 
+/**
+ * onLeanAngleUpdated
+ * ------------------
+ * Called with the latest lean angle.
+ */
+GuardianAngel.prototype.onLeanAngleUpdated = function(leanAngle) {
+    try {
+        // We use the current lean angle as the "center" angle until the Start
+        // biutton has been pressed, in case the device is not exactly central...
+        if(this.rideStarted === false) {
+            this.centeredLeanAngle = leanAngle;
+        }
+        leanAngle -= this.centeredLeanAngle;
+
+        // We only show the new angle on the dial if there has been a significant change...
+        if(Math.abs(leanAngle - this.previousLeanAngleDisplayed) > this.leanAngleDialSignificantChange) {
+            this.leanAngleDial.setValue(leanAngle);
+            this.previousLeanAngleDisplayed = leanAngle;
+        }
+
+        // We check if the new angle is greater than the maximum angles held
+        // for the current ride..
+        var absLean = Math.abs(leanAngle);
+        if(leanAngle < 0.0) {
+            if(absLean > this.maxLeftLean) {
+                this.maxLeftLean = absLean;
+                $("#max-left-lean").text(this.maxLeftLean.toFixed(1));
+            }
+        } else {
+            if(absLean > this.maxRightLean) {
+                this.maxRightLean = absLean;
+                $("#max-right-lean").text(this.maxRightLean.toFixed(1));
+            }
+        }
+    } catch(err) {
+        this.error(err.message);
+    }
+};
+
+/**
+ * onCrashDetected
+ * ---------------
+ * Called when a crash has been detected.
+ */
+GuardianAngel.prototype.onCrashDetected = function() {
+    try {
+
+    } catch(err) {
+        this.error(err.message);
+    }
+};
+
+/**
+ * error
+ * -----
+ * Logs an error.
+ */
+GuardianAngel.prototype.error = function(text) {
+    this.log("ERROR: " + text);
+};
+
+/**
+ * log
+ * ---
+ * Adds a log line to the logs slide.
+ */
+GuardianAngel.prototype.log = function(text) {
+    // We add the date/time to the message...
+    var message = new Date().toLocaleTimeString() + ": " + text;
+
+    var newElement = $("<div></div>");
+    newElement.addClass("log-line").html(message);
+    $("#log").prepend(newElement);
+};
+
+/**
+ * onStartButtonClicked
+ * --------------------
+ * Called when the Start button is clicked.
+ */
+GuardianAngel.prototype.onStartButtonClicked = function() {
+    try {
+        if(this.rideStarted) {
+            // We are currently running, so we stop...
+            this.stopRide();
+        } else {
+            // We are not running, so we start...
+            this.startRide();
+        }
+    } catch(err) {
+        this.error(err.message);
+    }
+};
+
+/**
+ * startRide
+ * ---------
+ * Starts monitoring a ride.
+ */
+GuardianAngel.prototype.startRide = function() {
+    // We change the button to say "Stop"...
+    $("#start-button").html('<i class="icon-stop"></i> Press to stop ');
+    $("#start-button").css("background-color", "green");
+
+    // We reset the max and min leans...
+    this.maxLeftLean = 0.0;
+    this.maxRightLean = 0.0;
+    $("#max-left-lean").text("0.0");
+    $("#max-right-lean").text("0.0");
+
+    this.rideStarted = true;
+};
+
+/**
+ * stopRide
+ * --------
+ * Stops monitoring a ride.
+ */
+GuardianAngel.prototype.stopRide = function() {
+    this.rideStarted = false;
+
+    // We change the button to say "Start"..
+    $("#start-button").html('<i class="icon-play"></i> Press to start');
+    $("#start-button").css("background-color", "red");
+
+    // We move to the ride-info slide...
+    this.swiper.slideTo(GuardianAngel.Slide.RIDE_INFO);
+};
